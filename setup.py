ec09fcffbe268e9a5be436d714ca1953c8d72b92
Ubuntu <ubuntu@ip-172-31-53-40.ec2.internal>
2022-10-25 19:08:47 +0000

    typo

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name='html_tbl_fmt',
       version='0.1',
       description='Format and conditional format of HTML tables',
-      packages=['html_tbl_fmt']
+      packages=['html_tbl_fmt'],
       install_requires=['lxml']
       )
